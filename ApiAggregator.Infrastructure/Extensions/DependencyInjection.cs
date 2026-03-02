e6331aa50c7e30ba2f83af2b49a2107bbbf0ebb0
athenaTounta <tounta.ath@gmail.com>
2026-03-02 04:42:48 +0200

    feat: add resilience with default settings

--- ApiAggregator.Infrastructure/Extensions/DependencyInjection.cs
+++ ApiAggregator.Infrastructure/Extensions/DependencyInjection.cs
@@ -2,18 +2,27 @@
 using ApiAggregator.Application.Abstractions;
 using ApiAggregator.Infrastructure.ExternalClients;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http.Resilience;
 
 namespace ApiAggregator.Infrastructure.Extensions
 {
     public static class DependencyInjection
     {
+        /// <summary>
+        /// left the client with the default resilience policy
+        /// 3 attempts with exponential backoff (2s, 4s, 8s) + jitter
+        /// 30s for entire request including retries
+        /// 10s timeout for each attempt
+        /// </summary>
         public static IServiceCollection AddInfrastructure(
             this IServiceCollection services)
         {
             services.AddHttpClient("ApiAggregator", client =>
             {
                 client.DefaultRequestHeaders.UserAgent.ParseAdd("ApiAggregator");
-            });
+            })
+            .AddStandardResilienceHandler();
+
             services.AddScoped<IExternalApiService, WeatherService>();
             services.AddScoped<IExternalApiService, NewsService>();
             services.AddScoped<IExternalApiService, GitHubService>();
